88172f567969f34d0b0d2903ac0cfcc097bea870
NightFyre <80198020+xCENTx@users.noreply.github.com>
2022-11-29 02:28:04 -0500

    Update Form1.cs (#2)

--- Form1.cs
+++ Form1.cs
@@ -7,66 +7,74 @@ namespace CheatEngineDataStructParser
 {
     public partial class NightFyreFrameworks : Form
     {
-        struct Tools
-        {
-            public static string Element(string description, string type, string offset)
-            {
-                /*
-                    <Element Offset="48" Vartype="Pointer" Bytesize="8" OffsetHex="00000030" Description="PlayerController" DisplayMethod="unsigned integer">
-                    - [0] Offset: 48
-                    - [1] Vartype: Pointer
-                    - [2] Bytesize: 8
-                    - [3] OffsetHex: 00000030
-                    - [4] Description: PlayerController
-                    - [5] DisplayMethod: unsigned integer
-                */
-                return $"   {GetType(type, description)};  //  0x{offset}\n";
-            }
-
-            public static string GetType(string input, string description)
-            {
-                //  Some dumped variables contain a '.' 
-                description = description.Replace(".", "");
-
-                // Var Type Redefinitons
-                //  Pointer             =   "Description"
-                //  Double              =   double
-                //  Float               =   float
-                //  4 Bytes             =   int32_t
-                //  byte                =   unsigned char
-                switch (input)
-                {
-                    case "Pointer": return $"{description}* {description}";
-                    case "Double": return $"double {description}";
-                    case "Float": return $"float {description}";
-                    case "4 Bytes": return $"int32_t {description}";
-                    case "Byte": return $"unsigned char {description}";
-                }
-                return $"[ERROR TYPE]{input}";
-            }
-        }
-
         public NightFyreFrameworks()
         {
             InitializeComponent();
         }
 
+        public static string ParseElement(XmlNode element)
+        {
+            // EXAMPLE NODE ATTRIBUTES
+            /*
+                <Element Offset="48" Vartype="Pointer" Bytesize="8" OffsetHex="00000030" Description="PlayerController" DisplayMethod="unsigned integer">
+                - [0] Offset: 48
+                - [1] Vartype: Pointer
+                - [2] Bytesize: 8
+                - [3] OffsetHex: 00000030
+                - [4] Description: PlayerController
+                - [5] DisplayMethod: unsigned integer
+            */
+            string Type     = "";                                               //  [1]
+            string Name     = element.Attributes[4].Value.Replace(".", "");     //  [4]
+            string Offset   = $"0x{element.Attributes[3].Value}";               //  [3]            
+
+            // If name contains "[]" , need to remove this from the string and add it onto offset
+            if (Name.Contains("["))
+            {
+                string buf = Name;                                      //  Clone Name
+                Name = Name.Remove(Name.IndexOf("["));                  //  Remove " [ DATA ] "
+                Offset += $" : {buf.Substring(buf.IndexOf('['))}";      //  Append offset with data
+            }
+
+            // Var Type Redefinitons
+            //  Pointer             =   "Description"
+            //  Double              =   double
+            //  Float               =   float
+            //  4 Bytes             =   int32_t
+            //  byte                =   unsigned char
+            switch (element.Attributes[1].Value)
+            {
+                case "Pointer":
+                    switch (element.HasChildNodes)  // Null Pointer Check
+                    {
+                        case true: Type = $"{element.ChildNodes[0].Attributes[0].Value.Replace(".", "")}*"; break;       //  Return Class Pointer
+                        case false: Type = $"{Name}*"; break;                                           //  Return VariableName
+                    }
+                    break;
+
+                case "Double": Type = "double"; break;
+                case "Float": Type = "float"; break;
+                case "4 Bytes": Type = "int32_t"; break;
+                case "Byte": Type = "unsigned char"; break;
+            }
+            
+            //  Get Formatted String
+            return $"   {Type} {Name}; //   {Offset}\n";
+        }
+
         private void ParseInput_MenuStripItem_Click(object sender, EventArgs e)
         {
-            string input = DataTable_RichTextBox.Text;                                                                  //  Obtain input data
-            XmlDocument doc = new XmlDocument();                                                                        //  Create XML Reader Instance
-            try { doc.LoadXml(input); }                                                                                 //  Try Loading the Input Data
-            catch (XmlException arg) { DataTable_RichTextBox.Text = arg.Message; }                                      //  Catch Error
+            string input = DataTable_RichTextBox.Text;                                                              //  Obtain input data
+            XmlDocument doc = new XmlDocument();                                                                    //  Create XML Reader Instance
+            try { doc.LoadXml(input); }                                                                             //  Try Loading the Input Data
+            catch (XmlException arg) { DataTable_RichTextBox.Text = arg.Message; }                                  //  Catch Error
             if (doc.DocumentElement == null) return;
 
             // Let the data parsing begin
-            try {
-                string buf = $"class {ClassNameEntry_MenuStrip.Text} {{\n";
-                foreach (XmlNode node in doc.DocumentElement)                                                               //  Loop Offsets
-                    buf += Tools.Element(node.Attributes[4].Value, node.Attributes[1].Value, node.Attributes[3].Value);     //  Parse Data 
-                DataTable_RichTextBox.Text = buf + "};";                                                                    //  Print data
-            }
-            catch (NullReferenceException arg) { DataTable_RichTextBox.Text = arg.Message; }
+            string buf = $"class {ClassNameEntry_MenuStrip.Text} {{\n";                                             //
+            foreach (XmlNode node in doc.DocumentElement)                                                           //  Loop Offsets
+                buf += ParseElement(node);                                                                          //  Parse Data 
+            DataTable_RichTextBox.Text = buf + "};";                                                                //  Print data
         }
 
         private void ClearDataTable_MEnuStripItem_Click(object sender, EventArgs e)
