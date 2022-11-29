145f82ca3ed305ae9e4dfe366a8071f24fd6b73a
NightFyre <80198020+xCENTx@users.noreply.github.com>
2022-11-28 21:13:01 -0500

    Initial commit (#1)

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CheatEngineDataStructParser
+{
+    public partial class NightFyreFrameworks : Form
+    {
+        struct Tools
+        {
+            public static string Element(string description, string type, string offset)
+            {
+                /*
+                    <Element Offset="48" Vartype="Pointer" Bytesize="8" OffsetHex="00000030" Description="PlayerController" DisplayMethod="unsigned integer">
+                    - [0] Offset: 48
+                    - [1] Vartype: Pointer
+                    - [2] Bytesize: 8
+                    - [3] OffsetHex: 00000030
+                    - [4] Description: PlayerController
+                    - [5] DisplayMethod: unsigned integer
+                */
+                return $"   {GetType(type, description)};  //  0x{offset}\n";
+            }
+
+            public static string GetType(string input, string description)
+            {
+                //  Some dumped variables contain a '.' 
+                description = description.Replace(".", "");
+
+                // Var Type Redefinitons
+                //  Pointer             =   "Description"
+                //  Double              =   double
+                //  Float               =   float
+                //  4 Bytes             =   int32_t
+                //  byte                =   unsigned char
+                switch (input)
+                {
+                    case "Pointer": return $"{description}* {description}";
+                    case "Double": return $"double {description}";
+                    case "Float": return $"float {description}";
+                    case "4 Bytes": return $"int32_t {description}";
+                    case "Byte": return $"unsigned char {description}";
+                }
+                return $"[ERROR TYPE]{input}";
+            }
+        }
+
+        public NightFyreFrameworks()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseInput_MenuStripItem_Click(object sender, EventArgs e)
+        {
+            string input = DataTable_RichTextBox.Text;                                                                  //  Obtain input data
+            XmlDocument doc = new XmlDocument();                                                                        //  Create XML Reader Instance
+            try { doc.LoadXml(input); }                                                                                 //  Try Loading the Input Data
+            catch (XmlException arg) { DataTable_RichTextBox.Text = arg.Message; }                                      //  Catch Error
+            if (doc.DocumentElement == null) return;
+
+            // Let the data parsing begin
+            try {
+                string buf = $"class {ClassNameEntry_MenuStrip.Text} {{\n";
+                foreach (XmlNode node in doc.DocumentElement)                                                               //  Loop Offsets
+                    buf += Tools.Element(node.Attributes[4].Value, node.Attributes[1].Value, node.Attributes[3].Value);     //  Parse Data 
+                DataTable_RichTextBox.Text = buf + "};";                                                                    //  Print data
+            }
+            catch (NullReferenceException arg) { DataTable_RichTextBox.Text = arg.Message; }
+        }
+
+        private void ClearDataTable_MEnuStripItem_Click(object sender, EventArgs e)
+        {
+            DataTable_RichTextBox.Clear();
+            ClassNameEntry_MenuStrip.Text = "<ClassName>";
+        }
+
+        private void ClassNameEntry_MenuStrip_Enter(object sender, EventArgs e)
+        {
+            if (ClassNameEntry_MenuStrip.Text == "<ClassName>")
+                ClassNameEntry_MenuStrip.Text = "";
+            ClassNameEntry_MenuStrip.ForeColor = Color.Black;
+        }
+
+        private void ClassNameEntry_MenuStrip_Leave(object sender, EventArgs e)
+        {
+            if (ClassNameEntry_MenuStrip.Text == "")
+                ClassNameEntry_MenuStrip.Text = "<ClassName>";
+            ClassNameEntry_MenuStrip.ForeColor = Color.Gray;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # CheatEngine-DataStructParser
+
+Example XML Input Data retrieved via GuidedHacking / CakeSan UnrealEngine Plugins
+The following can be obtained by copying the Data Structure Field in CheatEngine *see screenshot*
+<p align="center">
+<img src="https://user-images.githubusercontent.com/80198020/204420777-587e0e0a-fccf-4411-ad94-11d61273159c.png">
+</p>
+
+```xml
+<Elements>
+  <Element Offset="48" Vartype="Pointer" Bytesize="8" OffsetHex="00000030" Description="PlayerController" DisplayMethod="unsigned integer"/>
+  <Element Offset="56" Vartype="4 Bytes" Bytesize="4" OffsetHex="00000038" Description="CurrentNetSpeed" DisplayMethod="unsigned integer"/>
+  <Element Offset="60" Vartype="4 Bytes" Bytesize="4" OffsetHex="0000003C" Description="ConfiguredInternetSpeed" DisplayMethod="unsigned integer"/>
+  <Element Offset="64" Vartype="4 Bytes" Bytesize="4" OffsetHex="00000040" Description="ConfiguredLanSpeed" DisplayMethod="unsigned integer"/>
+  <Element Offset="88" Vartype="Pointer" Bytesize="8" OffsetHex="00000058" Description="ViewportClient" DisplayMethod="unsigned integer"/>
+  <Element Offset="124" Vartype="Byte" Bytesize="1" OffsetHex="0000007C" Description="AspectRatioAxisConstraint" DisplayMethod="unsigned integer"/>
+  <Element Offset="128" Vartype="Pointer" Bytesize="8" OffsetHex="00000080" Description="PendingLevelPlayerControllerClass" DisplayMethod="unsigned integer"/>
+  <Element Offset="136" Vartype="Byte" Bytesize="1" OffsetHex="00000088" Description="bSentSplitJoin[1]" DisplayMethod="unsigned integer"/>
+</Elements>
+```
+
+**Output Result**
+```c++
+class LocalPlayer {
+   PlayerController* PlayerController;  //  0x00000030
+   int32_t CurrentNetSpeed;  //  0x00000038
+   int32_t ConfiguredInternetSpeed;  //  0x0000003C
+   int32_t ConfiguredLanSpeed;  //  0x00000040
+   ViewportClient* ViewportClient;  //  0x00000058
+   unsigned char AspectRatioAxisConstraint;  //  0x0000007C
+   PendingLevelPlayerControllerClass* PendingLevelPlayerControllerClass;  //  0x00000080
+   unsigned char bSentSplitJoin[1];  //  0x00000088
+};
+```
